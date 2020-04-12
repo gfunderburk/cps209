ccdad03af598eb0c23b4241c47c7456ddc43dfcf
Jonathan David Layton <jonathanlayton99@gmail.com>
2020-04-11 21:44:11 -0400

    UnitTest barebones

---
+++ src/test/java/EntityHumanoidTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+import Game_model.*;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// EntityHumanoidTest.java is the primary file to test Game functionality. 
+// Since most all of the game logic hinges upon responding to projectile collisions, 
+//    little if anything can be tested without projectile functionality. 
+
+public class EntityHumanoidTest {
+
+    @Test
+    public void test_EntityHumanoidCreation()   throws IOException 
+    {
+
+    }
+
+    @Test
+    public void test_EntityHumanoidDeathEvent()   throws IOException 
+    {
+
+    }
+
+    @Test
+    public void test_EntityHumanoidAttack()   throws IOException 
+    {
+
+    }
+
+    @Test
+    public void test_EntityHumanoidMove()   throws IOException 
+    {
+
+    }
+
+}
+
+
+
+
+
---
+++ src/test/java/EntityKillableTest.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import Game_model.*;
+import Game_model.Entity;
+import Game_model.Game;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// EntityKillableTest.java is the primary file to test Game functionality. 
+// Since most all of the game logic hinges upon responding to projectile collisions, 
+//    little if anything can be tested without projectile functionality. 
+
+public class EntityKillableTest {
+
+
+    ArrayList<Entity> testList = new ArrayList<Entity>(Arrays.asList(
+        new EK_Scenery(),
+        new EK_Scenery(),
+        new EK_Scenery(),
+        new EK_Scenery()
+    ));
+
+    @Test
+    public void test_EntityDeath() 
+    {
+        Game.getIt().setEntityList(testList);
+
+        assertEquals(4, Game.getIt().getEntityList().size());
+
+        var testItem = (EK_Scenery)Game.getIt().getEntityList().get(0); // Simulate projectile striking and killing Entity, the Entity will be removed from the Entity List.
+        testItem.deathEvent(); 
+
+        assertEquals(3, Game.getIt().getEntityList().size());
+    }
+
+    @Test
+    public void test_GameOverCheck()   throws IOException 
+    {
+        Game.getIt().setHostilesLeft(5);
+        
+        assertEquals(false, Game.getIt().isGameOver());
+
+        Game.getIt().setHostilesLeft(0);
+
+        Game.getIt().checkGameOver();  // Should detect a gameOver event and change game state accordingly.
+
+        assertEquals(true, Game.getIt().isGameOver());
+    }
+
+}
+
+
+
+
+
